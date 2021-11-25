2ea7343e034100d29178a2daf8bbd5bc10680e7e
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-25 18:31:28 +0100

    Create Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+    <li class="text-center">
+        <div class="ms-card">
+            <div class="cover">
+                <img :src="img" alt="title">
+            </div>
+            <div class="text">
+                <h3>{{ title }}</h3>
+                <h4>{{ subtitle }}</h4>
+                <span>{{ year }}</span>
+                <h6>{{ span }}</h6>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        img: String,
+        title: String,
+        subtitle: String,
+        year: String,
+        span: String,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+
+li {
+    width: calc((100% / 8) - 35px);
+    margin: 0 17.5px;
+    padding: 20px;
+    background-color: $bg_secondary;
+    .text {
+        h3,
+        h6 {
+            color: $color_primary;
+        }
+        h4,
+        span {
+            color: $color_secondary;
+        }
+    }
+}
+
+</style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,53 +1,32 @@
 <template>
     <section class="list ms-container d-flex justify-content-center align-items-center h-100">
         <ul class="row col-12 justify-content-center flex-wrap">
-            <li class="text-center">
-                <div class="ms-card">
-                    <div class="cover">
-                        <img src="../assets/logo (1).png" alt="">
-                    </div>
-                    <div class="text">
-                        <h3>Title</h3>
-                        <h4>Subtitle</h4>
-                        <span>span</span>
-                        <h6>span</h6>
-                    </div>
-                </div>
-            </li>
+            <Card 
+                :img="require(`../assets/logo (1).png`)" 
+                title="title" 
+                subtitle="subtitle" 
+                year="year" 
+                span="span"
+            />
         </ul>
     </section>
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
 export default {
     name: 'CardList',
+    components: {
+        Card,
+    }
 }
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/colors.scss';
-
 .list {
     &.ms-container {
         max-width: 1920px;
         margin: 0 80px;
-    }
-    color: #fff;
-    li {
-        width: calc((100% / 8) - 35px);
-        margin: 0 17.5px;
-        padding: 20px;
-        background-color: $bg_secondary;
-        .text {
-            h3,
-            h6 {
-                color: $color_primary;
-            }
-            h4,
-            span {
-                color: $color_secondary;
-            }
-        }
     }
 }
 
