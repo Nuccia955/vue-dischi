c1451beb4e7fb50b7dacd117279147fc46e37aaf
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-25 17:31:07 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
     <div id="app">
-        
+        <Header />
     </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue'
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  @import '~bootstrap/scss/bootstrap.scss';
-  
+@import '~bootstrap/scss/bootstrap.scss';
+@import '@/styles/colors.scss';
+
+body {
+  background-color: $bg_primary;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="p-1">
+        <img src="../assets/logo (1).png" alt="Spotify Logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+header {
+    background-color: $bg_secondary;
+    img {
+        width: 40px;
+    }
+}
+</style>
